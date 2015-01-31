d2ad852104a52ad8b613a1a393b7304320cf9e3e
Ian Whitney <whit0694@umn.edu>
2015-01-31 10:05:51 -0600

    test cleanup

--- robot_name_test.rb
+++ robot_name_test.rb
@@ -3,9 +3,7 @@ require_relative 'robot'
 
 class RobotTest < MiniTest::Unit::TestCase
   def test_has_name
-    # rubocop:disable Lint/AmbiguousRegexpLiteral
-    assert_match /^[a-zA-Z]{2}\d{3}$/, Robot.new.name
-    # rubocop:enable Lint/AmbiguousRegexpLiteral
+    assert_match /^[A-Z]{2}\d{3}$/, Robot.new.name
   end
 
   def test_name_sticks
@@ -15,9 +13,7 @@ class RobotTest < MiniTest::Unit::TestCase
   end
 
   def test_different_robots_have_different_names
-    # rubocop:disable Lint/UselessComparison
     assert Robot.new.name != Robot.new.name
-    # rubocop:enable Lint/UselessComparison
   end
 
   def test_reset_name
@@ -26,8 +22,6 @@ class RobotTest < MiniTest::Unit::TestCase
     robot.reset
     name2 = robot.name
     assert name != name2
-    # rubocop:disable Lint/AmbiguousRegexpLiteral
-    assert_match /^[a-zA-Z]{2}\d{3}$/, name2
-    # rubocop:enable Lint/AmbiguousRegexpLiteral
+    assert_match /^[A-Z]{2}\d{3}$/, name2
   end
 end
